e37b95fd365601dc1b19c0907f524eac35a15c4c
Irina <golubkovairina76@gmail.com>
2022-01-28 23:24:40 +0300

    Learnup

--- src/Main/java1.java
+++ src/Main/java1.java
@@ -2,6 +2,6 @@ package Main;
 
 public class java1 {
     public static void main(String[] args) {
-        System.out.println("Hello Learnup!");
+        System.out.println("Hello World!");
     }
 }
