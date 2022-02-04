c90c70ea8640b6d098a81e39305717e92062bc01
Irina <golubkovairina76@gmail.com>
2022-02-04 21:20:11 +0300

    new task

---
+++ src/Main/main16.java
@@ -0,0 +1,29 @@
+package Main;
+
+public class main16 {
+
+    public static void printCountOfDroppedOutPlayers(int speedOfPlayer1, int speedOfPlayer2, int speedOfPlayer3) {
+        int countOfPlayers = 0;
+        if (DroppedOutPlayerCalculator.isPlayerDroppedOut(speedOfPlayer1)) {
+            ++countOfPlayers;
+        }
+        if (DroppedOutPlayerCalculator.isPlayerDroppedOut(speedOfPlayer2)) {
+            ++countOfPlayers;
+        }
+        if (DroppedOutPlayerCalculator.isPlayerDroppedOut(speedOfPlayer3)) {
+            ++countOfPlayers;
+        }
+        System.out.println("Количество выбывших игроков: " + countOfPlayers);
+    }
+
+    public static void main(String[] args) {
+
+        DroppedOutPlayerCalculator.isGreenLight = false;
+
+        printCountOfDroppedOutPlayers(1, 0, 2);
+    }
+}
+//Оформите игру в виде отдельного класса, который бы в статическом поле хранил
+//состояние светофора (включён ли красный свет или зелёный свет) и имел бы статический метод,
+//принимащий параметром скорость и возвращающий ответ на вопрос,
+//выбыл ли игрок с этой скоростью при текущем свете светофора.
